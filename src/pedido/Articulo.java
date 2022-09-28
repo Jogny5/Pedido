b190d75c0e7b7f2fef8418cbc2ef9730cffbbb46
ElPerroGuaton <dgutierrez2020@udec.cl>
2022-09-28 19:09:34 -0300

    Clase Articulo

---
+++ src/pedido/Articulo.java
@@ -0,0 +1,16 @@
+
+package pedido;
+
+
+public class Articulo {
+    
+    private float peso;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+  
+    public Articulo(){
+        
+    }
+    
+}

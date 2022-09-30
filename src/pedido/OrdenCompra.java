c69708501676c05af44308ab7659866ef8225119
Jogny5 <114445890+Jogny5@users.noreply.github.com>
2022-09-30 15:13:53 -0300

    cambios main y constructor de ordencompra

--- src/pedido/OrdenCompra.java
+++ src/pedido/OrdenCompra.java
@@ -5,16 +5,39 @@ import java.util.Date;
 
 public class OrdenCompra {
     
+    private Articulo articulo1;
+    private Articulo articulo2;
+    private Articulo articulo3;
+    private Articulo articulo4;
+    private Articulo articulo5;
+    private int cantidadA;
+    private int cantidadB;
+    private int cantidadC;
+    private int cantidadD;
+    private int cantidadE;
     private Date fecha;
     private String estado;
     public DocTributario doc;
     private Cliente cliente;
     public ArrayList<DetalleOrden> ar;
     
-    public OrdenCompra(Date hoy,String state,Cliente c){
+    public OrdenCompra(Date hoy,String state,Cliente client,
+            Articulo a,int cantA,Articulo b,int cantB,
+            Articulo c,int cantC,Articulo d,int cantD,
+            Articulo e,int cantE){
+        this.articulo1=a;
+        this.articulo2=b;
+        this.articulo3=c;
+        this.articulo4=d;
+        this.articulo5=e;
+        this.cantidadA=cantA;
+        this.cantidadB=cantB;
+        this.cantidadC=cantC;
+        this.cantidadD=cantD;
+        this.cantidadE=cantE;
         this.fecha=hoy;
         this.estado=state;
-        this.cliente=c;
+        this.cliente=client;
     }
     void setDocTributario(DocTributario doct){
         doc=doct;
